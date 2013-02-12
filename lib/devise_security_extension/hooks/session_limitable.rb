db9e1f5e50868f56aec2df432b113db7fca9dc1e
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2013-02-12 14:13:03 +0100

    Enable multiple session (and one session).

--- lib/devise_security_extension/hooks/session_limitable.rb
+++ lib/devise_security_extension/hooks/session_limitable.rb
@@ -3,10 +3,12 @@
 # and on authentication. Retrieving the user from session (:fetch) does
 # not trigger it.
 Warden::Manager.after_set_user :except => :fetch do |record, warden, options|
-  if record.respond_to?(:update_unique_session_id!) && warden.authenticated?(options[:scope])
-    unique_session_id = Devise.friendly_token
-    warden.session(options[:scope])['unique_session_id'] = unique_session_id
-    record.update_unique_session_id!(unique_session_id)
+  if record.respond_to?(:single_session?) && record.single_session?
+    if record.respond_to?(:update_unique_session_id!) && warden.authenticated?(options[:scope])
+      unique_session_id = Devise.friendly_token
+      warden.session(options[:scope])['unique_session_id'] = unique_session_id
+      record.update_unique_session_id!(unique_session_id)
+    end
   end
 end
 
@@ -17,10 +19,12 @@ Warden::Manager.after_set_user :only => :fetch do |record, warden, options|
   scope = options[:scope]
   env   = warden.request.env
 
-  if warden.authenticated?(scope) && options[:store] != false
-    if record.unique_session_id != warden.session(scope)['unique_session_id'] && !env['devise.skip_session_limitable']
-      warden.logout(scope)
-      throw :warden, :scope => scope, :message => :session_limited
+  if record.respond_to?(:single_session?) && record.single_session?
+    if warden.authenticated?(scope) && options[:store] != false
+      if record.unique_session_id != warden.session(scope)['unique_session_id'] && !env['devise.skip_session_limitable']
+        warden.logout(scope)
+        throw :warden, :scope => scope, :message => :session_limited
+      end
     end
   end
 end
