aa50201a6eec9caf236d5954129dbe77bb9df791
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2013-02-12 16:28:11 +0100

    Simplify ifs

--- lib/devise_security_extension/hooks/session_limitable.rb
+++ lib/devise_security_extension/hooks/session_limitable.rb
@@ -3,12 +3,11 @@
 # and on authentication. Retrieving the user from session (:fetch) does
 # not trigger it.
 Warden::Manager.after_set_user :except => :fetch do |record, warden, options|
-  if record.respond_to?(:single_session?) && record.single_session?
-    if record.respond_to?(:update_unique_session_id!) && warden.authenticated?(options[:scope])
-      unique_session_id = Devise.friendly_token
-      warden.session(options[:scope])['unique_session_id'] = unique_session_id
-      record.update_unique_session_id!(unique_session_id)
-    end
+  if record.respond_to?(:update_unique_session_id!) && warden.authenticated?(options[:scope])
+   record.respond_to?(:single_session?) && record.single_session?
+    unique_session_id = Devise.friendly_token
+    warden.session(options[:scope])['unique_session_id'] = unique_session_id
+    record.update_unique_session_id!(unique_session_id)
   end
 end
 
@@ -19,12 +18,11 @@ Warden::Manager.after_set_user :only => :fetch do |record, warden, options|
   scope = options[:scope]
   env   = warden.request.env
 
-  if record.respond_to?(:single_session?) && record.single_session?
-    if warden.authenticated?(scope) && options[:store] != false
-      if record.unique_session_id != warden.session(scope)['unique_session_id'] && !env['devise.skip_session_limitable']
-        warden.logout(scope)
-        throw :warden, :scope => scope, :message => :session_limited
-      end
+  if warden.authenticated?(scope) && options[:store] != false &&
+    record.respond_to?(:single_session?) && record.single_session?
+    if record.unique_session_id != warden.session(scope)['unique_session_id'] && !env['devise.skip_session_limitable']
+      warden.logout(scope)
+      throw :warden, :scope => scope, :message => :session_limited
     end
   end
 end
